6b7a2d8980cd710887c90397883b5e3ba334fdf2
anshul 524 <anshul0524@gmail.com>
2024-08-20 14:41:04 +0530

    text-delet

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const navbarlinks = () => {
-  document.getElementById("nav-btn").classList.toggle("show-nav");
-  document.body.classList.toggle("overflow-hidden");
-  document.getElementById("ROT-1").classList.toggle("rotated");
-  document.getElementById("ROT-2").classList.toggle("rotated-2");
-  document.getElementById("d-n").classList.toggle("bg-transparent");
-};
